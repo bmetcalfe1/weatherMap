2ee6a82dc74d3b3be4ed8c8e644e41b6c173d1a5
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-12-19 17:29:36 -0500

    geolocation

--- weatherMap.js
+++ weatherMap.js
@@ -1,197 +1,205 @@
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 45.5017, lng: -73.5673},
-    zoom: 13,
-    mapTypeId: 'roadmap'
-  });
+function onLoad() {
+	var montreal = {lat: 45.5017, lng: -73.5673};
+	var toronto = {lat: 43.652644, lng: -79.381769};
 
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-    console.log("Place", place);
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-      getWeather(place.formatted_address);
-      $('.locale').text(place.name);
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-}
-
-
-function addIcon(weather) {
-// for loop to add hide to all div icons children
-// addIcon used only in iconGen
-	var div = document.getElementById('icons');
-	var divChildren = div.childNodes; // get an array of child nodes
-
-	for (var i=0; i<divChildren.length; i++) {
-	    divChildren[i].className += " hide";
+	function getLocation() {
+		navigator.geolocation.getCurrentPosition(showLocation);
 	}
-	if ($('div.' + weather).hasClass('hide')) {
-	  $('div.' + weather).removeClass('hide');
+
+	function showLocation( position ) {
+		var latty = position.coords.latitude;
+		var longy = position.coords.longitude;
+		var newLocal = {lat: latty, lng: longy};
+		console.log(newLocal);
 	}
-}
 
-function iconGen(weather) {
-	console.log(weather);
-	var weather = weather.toLowerCase();
-	switch (weather) {
-	  case 'drizzle':
-	    addIcon(weather)
-	    break;
-	  case 'clouds':
-	    addIcon(weather)
-	    break;
-	  case 'fog':
-	    addIcon(weather)
-	    break;
-	  case 'haze':
-	    addIcon(weather)
-	    break;
-	  case 'rain':
-	    addIcon(weather)
-	    break;
-	  case 'snow':
-	    addIcon(weather)
-	    break;
-	  case 'clear':
-	    addIcon(weather)
-	    break;
-	  case 'mist':
-	    addIcon(weather)
-	    break;
-	  case 'thunderstorm':
-	    addIcon(weather)
-	    break;
-	  default:
-	    console.log("defaulting");
-	    $('div.clouds').removeClass('hide');
+	var map = new google.maps.Map(document.getElementById('map'), {
+	center: toronto,
+	zoom: 13,
+	mapTypeId: 'roadmap'
+	});
+
+	// Create the search box and link it to the UI element.
+	var input = document.getElementById('pac-input');
+	var searchBox = new google.maps.places.SearchBox(input);
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+	// Bias the SearchBox results towards current map's viewport.
+	map.addListener('bounds_changed', function() {
+	searchBox.setBounds(map.getBounds());
+	});
+
+	var markers = [];
+	// Listen for the event fired when the user selects a prediction and retrieve
+	// more details for that place.
+	searchBox.addListener('places_changed', function() {
+	var places = searchBox.getPlaces();
+
+	if (places.length == 0) {
+	  return;
 	}
-}
 
-function getWeather(city) {
-	console.log(city);
+	// Clear out the old markers.
+	markers.forEach(function(marker) {
+	  marker.setMap(null);
+	});
+	markers = [];
 
-	var weatherKey = "3d116075e0fe88576d7d105ffb94897e";
-	var weatherApiLink = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherKey;
-
-	$.getJSON(weatherApiLink, function(response) {
-	  // need some error handling...
-	  var theWeather = response.weather[0].main;
-	  iconGen(theWeather);
-	  // weatherPhoto(theWeather);
-
-	  var theDescription = response.weather[0].description;
-	  function capitalizeFirst(string) {
-	      var newDesc = string.charAt(0).toUpperCase() + string.slice(1);
-	      $('.detail-description').text(newDesc);
+	// For each place, get the icon, name and location.
+	var bounds = new google.maps.LatLngBounds();
+	places.forEach(function(place) {
+	console.log("Place", place);
+	  if (!place.geometry) {
+	    console.log("Returned place contains no geometry");
+	    return;
 	  }
-	  capitalizeFirst(theDescription);
+	  getWeather(place.formatted_address);
+	  $('.locale').text(place.name);
+	  var icon = {
+	    url: place.icon,
+	    size: new google.maps.Size(71, 71),
+	    origin: new google.maps.Point(0, 0),
+	    anchor: new google.maps.Point(17, 34),
+	    scaledSize: new google.maps.Size(25, 25)
+	  };
 
-	  var tempCel = ((response.main.temp - 273.15).toFixed(2) + " °C");
-	  var tempFar = ((( response.main.temp - 273.15) * 9/5) + 32).toFixed(2) + " °F";
-	  //countries where farhenheit
-	  // if (country === "US" ||
-	  //     country === "BZ" ||
-	  //     country === "KY" ||
-	  //     country === "GU" ||
-	  //     country === "PR" ||
-	  //     country === "PW" ||
-	  //     country === "VA") 
-	  // {
-	  //   $('.temps').text(tempFar);  
-	  // }
-	  // else {
-	  //   $('.temps').text(tempCel);
-	  // }
-	  $('.temps').text(tempCel);
-	  $('.weather-data').removeClass('hide');
-	  // ***
-	  // sun-data
-	  // ***
-	  var parsedSunriseHours = new Date(response.sys.sunrise*1000).getHours();
-	    if (parsedSunriseHours < 10) {
-	      parsedSunriseHours = "0" + parsedSunriseHours;
-	    }
-	  var parsedSunriseMins = new Date(response.sys.sunrise*1000).getMinutes();
-	    if (parsedSunriseMins < 10) {
-	      parsedSunriseMins = "0" + parsedSunriseMins;
-	    }
-	  var parsedSunsetHours = new Date(response.sys.sunset*1000).getHours();
-	    if (parsedSunsetHours < 10) {
-	      parsedSunsetHours = "0" + parsedSunsetHours;
-	    }
-	  var parsedSunsetMins = new Date(response.sys.sunset*1000).getMinutes();
-	    if (parsedSunsetMins < 10) {
-	      parsedSunsetMins = "0" + parsedSunsetMins;
-	    }
-	  var formatSunrise = parsedSunriseHours + ":" + parsedSunriseMins;
-	  var formatSunset = parsedSunsetHours + ":" + parsedSunsetMins; 
-	  // Next: Make accept other time zones
+	  // Create a marker for each place.
+	  markers.push(new google.maps.Marker({
+	    map: map,
+	    icon: icon,
+	    title: place.name,
+	    position: place.geometry.location
+	  }));
 
-	  $('.sunrise').text(formatSunrise);
-	  $('.sunset').text(formatSunset);
-	  $('.sun-data').removeClass('hide');
-	  });
+	  if (place.geometry.viewport) {
+	    // Only geocodes have viewport.
+	    bounds.union(place.geometry.viewport);
+	  } else {
+	    bounds.extend(place.geometry.location);
+	  }
+	});
+	map.fitBounds(bounds);
+	});
 }
+
+	function addIcon(weather) {
+	// for loop to add hide to all div icons children
+	// addIcon used only in iconGen
+		var div = document.getElementById('icons');
+		var divChildren = div.childNodes; // get an array of child nodes
+
+		for (var i=0; i<divChildren.length; i++) {
+		    divChildren[i].className += " hide";
+		}
+		if ($('div.' + weather).hasClass('hide')) {
+		  $('div.' + weather).removeClass('hide');
+		}
+	}
+
+	function iconGen(weather) {
+		console.log(weather);
+		var weather = weather.toLowerCase();
+		switch (weather) {
+		  case 'drizzle':
+		    addIcon(weather)
+		    break;
+		  case 'clouds':
+		    addIcon(weather)
+		    break;
+		  case 'fog':
+		    addIcon(weather)
+		    break;
+		  case 'haze':
+		    addIcon(weather)
+		    break;
+		  case 'rain':
+		    addIcon(weather)
+		    break;
+		  case 'snow':
+		    addIcon(weather)
+		    break;
+		  case 'clear':
+		    addIcon(weather)
+		    break;
+		  case 'mist':
+		    addIcon(weather)
+		    break;
+		  case 'thunderstorm':
+		    addIcon(weather)
+		    break;
+		  default:
+		    console.log("defaulting");
+		    $('div.clouds').removeClass('hide');
+		}
+	}
+
+	function getWeather(city) {
+		console.log(city);
+
+		var weatherKey = "3d116075e0fe88576d7d105ffb94897e";
+		var weatherApiLink = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherKey;
+
+		$.getJSON(weatherApiLink, function(response) {
+		  // need some error handling...
+		  var theWeather = response.weather[0].main;
+		  iconGen(theWeather);
+		  // weatherPhoto(theWeather);
+
+		  var theDescription = response.weather[0].description;
+		  function capitalizeFirst(string) {
+		      var newDesc = string.charAt(0).toUpperCase() + string.slice(1);
+		      $('.detail-description').text(newDesc);
+		  }
+		  capitalizeFirst(theDescription);
+
+		  var tempCel = ((response.main.temp - 273.15).toFixed(2) + " °C");
+		  var tempFar = ((( response.main.temp - 273.15) * 9/5) + 32).toFixed(2) + " °F";
+		  //countries where farhenheit
+		  // if (country === "US" ||
+		  //     country === "BZ" ||
+		  //     country === "KY" ||
+		  //     country === "GU" ||
+		  //     country === "PR" ||
+		  //     country === "PW" ||
+		  //     country === "VA") 
+		  // {
+		  //   $('.temps').text(tempFar);  
+		  // }
+		  // else {
+		  //   $('.temps').text(tempCel);
+		  // }
+		  $('.temps').text(tempCel);
+		  $('.weather-data').removeClass('hide');
+		  // ***
+		  // sun-data
+		  // ***
+		  var parsedSunriseHours = new Date(response.sys.sunrise*1000).getHours();
+		    if (parsedSunriseHours < 10) {
+		      parsedSunriseHours = "0" + parsedSunriseHours;
+		    }
+		  var parsedSunriseMins = new Date(response.sys.sunrise*1000).getMinutes();
+		    if (parsedSunriseMins < 10) {
+		      parsedSunriseMins = "0" + parsedSunriseMins;
+		    }
+		  var parsedSunsetHours = new Date(response.sys.sunset*1000).getHours();
+		    if (parsedSunsetHours < 10) {
+		      parsedSunsetHours = "0" + parsedSunsetHours;
+		    }
+		  var parsedSunsetMins = new Date(response.sys.sunset*1000).getMinutes();
+		    if (parsedSunsetMins < 10) {
+		      parsedSunsetMins = "0" + parsedSunsetMins;
+		    }
+		  var formatSunrise = parsedSunriseHours + ":" + parsedSunriseMins;
+		  var formatSunset = parsedSunsetHours + ":" + parsedSunsetMins; 
+		  // Next: Make accept other time zones
+
+		  $('.sunrise').text(formatSunrise);
+		  $('.sunset').text(formatSunset);
+		  $('.sun-data').removeClass('hide');
+		  });
+	}
 
 // to do
 
-// improve data with country code
-// if searching, say, a bar with name of a city, will return that cities data
-// or if search london ontario, will return london UK
 // proper time zones
-// if not city, still need to return something
-// add css
 // idea: find language of searched country, return translated data (and can toggle lang)
