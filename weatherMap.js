b17523e3569f40baa773255d29c91f8cb8e2d84d
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-12-19 17:43:37 -0500

    formatting

--- weatherMap.js
+++ weatherMap.js
@@ -2,21 +2,10 @@ function onLoad() {
 	var montreal = {lat: 45.5017, lng: -73.5673};
 	var toronto = {lat: 43.652644, lng: -79.381769};
 
-	function getLocation() {
-		navigator.geolocation.getCurrentPosition(showLocation);
-	}
-
-	function showLocation( position ) {
-		var latty = position.coords.latitude;
-		var longy = position.coords.longitude;
-		var newLocal = {lat: latty, lng: longy};
-		console.log(newLocal);
-	}
-
 	var map = new google.maps.Map(document.getElementById('map'), {
-	center: toronto,
-	zoom: 13,
-	mapTypeId: 'roadmap'
+		center: toronto,
+		zoom: 13,
+		mapTypeId: 'roadmap'
 	});
 
 	// Create the search box and link it to the UI element.
@@ -82,124 +71,137 @@ function onLoad() {
 	});
 }
 
-	function addIcon(weather) {
+function getLocation() {
+	navigator.geolocation.getCurrentPosition(showLocation);
+}
+
+function showLocation( position ) {
+	var latty = position.coords.latitude;
+	var longy = position.coords.longitude;
+	var newLocal = {lat: latty, lng: longy};
+	return newLocal;
+}
+
+function addIcon(weather) {
+	var div = document.getElementById('icons');
+	var divChildren = div.childNodes; // get an array of child nodes
+
 	// for loop to add hide to all div icons children
-	// addIcon used only in iconGen
-		var div = document.getElementById('icons');
-		var divChildren = div.childNodes; // get an array of child nodes
 
-		for (var i=0; i<divChildren.length; i++) {
-		    divChildren[i].className += " hide";
-		}
-		if ($('div.' + weather).hasClass('hide')) {
-		  $('div.' + weather).removeClass('hide');
-		}
+	for (var i=0; i<divChildren.length; i++) {
+	    divChildren[i].className += " hide";
 	}
-
-	function iconGen(weather) {
-		console.log(weather);
-		var weather = weather.toLowerCase();
-		switch (weather) {
-		  case 'drizzle':
-		    addIcon(weather)
-		    break;
-		  case 'clouds':
-		    addIcon(weather)
-		    break;
-		  case 'fog':
-		    addIcon(weather)
-		    break;
-		  case 'haze':
-		    addIcon(weather)
-		    break;
-		  case 'rain':
-		    addIcon(weather)
-		    break;
-		  case 'snow':
-		    addIcon(weather)
-		    break;
-		  case 'clear':
-		    addIcon(weather)
-		    break;
-		  case 'mist':
-		    addIcon(weather)
-		    break;
-		  case 'thunderstorm':
-		    addIcon(weather)
-		    break;
-		  default:
-		    console.log("defaulting");
-		    $('div.clouds').removeClass('hide');
-		}
+	if ($('div.' + weather).hasClass('hide')) {
+	  $('div.' + weather).removeClass('hide');
 	}
+}
 
-	function getWeather(city) {
-		console.log(city);
-
-		var weatherKey = "3d116075e0fe88576d7d105ffb94897e";
-		var weatherApiLink = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherKey;
-
-		$.getJSON(weatherApiLink, function(response) {
-		  // need some error handling...
-		  var theWeather = response.weather[0].main;
-		  iconGen(theWeather);
-		  // weatherPhoto(theWeather);
-
-		  var theDescription = response.weather[0].description;
-		  function capitalizeFirst(string) {
-		      var newDesc = string.charAt(0).toUpperCase() + string.slice(1);
-		      $('.detail-description').text(newDesc);
-		  }
-		  capitalizeFirst(theDescription);
-
-		  var tempCel = ((response.main.temp - 273.15).toFixed(2) + " °C");
-		  var tempFar = ((( response.main.temp - 273.15) * 9/5) + 32).toFixed(2) + " °F";
-		  //countries where farhenheit
-		  // if (country === "US" ||
-		  //     country === "BZ" ||
-		  //     country === "KY" ||
-		  //     country === "GU" ||
-		  //     country === "PR" ||
-		  //     country === "PW" ||
-		  //     country === "VA") 
-		  // {
-		  //   $('.temps').text(tempFar);  
-		  // }
-		  // else {
-		  //   $('.temps').text(tempCel);
-		  // }
-		  $('.temps').text(tempCel);
-		  $('.weather-data').removeClass('hide');
-		  // ***
-		  // sun-data
-		  // ***
-		  var parsedSunriseHours = new Date(response.sys.sunrise*1000).getHours();
-		    if (parsedSunriseHours < 10) {
-		      parsedSunriseHours = "0" + parsedSunriseHours;
-		    }
-		  var parsedSunriseMins = new Date(response.sys.sunrise*1000).getMinutes();
-		    if (parsedSunriseMins < 10) {
-		      parsedSunriseMins = "0" + parsedSunriseMins;
-		    }
-		  var parsedSunsetHours = new Date(response.sys.sunset*1000).getHours();
-		    if (parsedSunsetHours < 10) {
-		      parsedSunsetHours = "0" + parsedSunsetHours;
-		    }
-		  var parsedSunsetMins = new Date(response.sys.sunset*1000).getMinutes();
-		    if (parsedSunsetMins < 10) {
-		      parsedSunsetMins = "0" + parsedSunsetMins;
-		    }
-		  var formatSunrise = parsedSunriseHours + ":" + parsedSunriseMins;
-		  var formatSunset = parsedSunsetHours + ":" + parsedSunsetMins; 
-		  // Next: Make accept other time zones
-
-		  $('.sunrise').text(formatSunrise);
-		  $('.sunset').text(formatSunset);
-		  $('.sun-data').removeClass('hide');
-		  });
+function iconGen(weather) {
+	console.log(weather);
+	var weather = weather.toLowerCase();
+	switch (weather) {
+	  case 'drizzle':
+	    addIcon(weather)
+	    break;
+	  case 'clouds':
+	    addIcon(weather)
+	    break;
+	  case 'fog':
+	    addIcon(weather)
+	    break;
+	  case 'haze':
+	    addIcon(weather)
+	    break;
+	  case 'rain':
+	    addIcon(weather)
+	    break;
+	  case 'snow':
+	    addIcon(weather)
+	    break;
+	  case 'clear':
+	    addIcon(weather)
+	    break;
+	  case 'mist':
+	    addIcon(weather)
+	    break;
+	  case 'thunderstorm':
+	    addIcon(weather)
+	    break;
+	  default:
+	    console.log("defaulting");
+	    $('div.clouds').removeClass('hide');
 	}
+}
+
+function getWeather(city) {
+	console.log(city);
+
+	var weatherKey = "3d116075e0fe88576d7d105ffb94897e";
+	var weatherApiLink = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherKey;
+
+	$.getJSON(weatherApiLink, function(response) {
+	  // need some error handling...
+	  var theWeather = response.weather[0].main;
+	  iconGen(theWeather);
+	  // weatherPhoto(theWeather);
+
+	  var theDescription = response.weather[0].description;
+	  function capitalizeFirst(string) {
+	      var newDesc = string.charAt(0).toUpperCase() + string.slice(1);
+	      $('.detail-description').text(newDesc);
+	  }
+	  capitalizeFirst(theDescription);
+
+	  var tempCel = ((response.main.temp - 273.15).toFixed(2) + " °C");
+	  var tempFar = ((( response.main.temp - 273.15) * 9/5) + 32).toFixed(2) + " °F";
+	  //countries where farhenheit
+	  // if (country === "US" ||
+	  //     country === "BZ" ||
+	  //     country === "KY" ||
+	  //     country === "GU" ||
+	  //     country === "PR" ||
+	  //     country === "PW" ||
+	  //     country === "VA") 
+	  // {
+	  //   $('.temps').text(tempFar);  
+	  // }
+	  // else {
+	  //   $('.temps').text(tempCel);
+	  // }
+	  $('.temps').text(tempCel);
+	  $('.weather-data').removeClass('hide');
+	  // ***
+	  // sun-data
+	  // ***
+	  var parsedSunriseHours = new Date(response.sys.sunrise*1000).getHours();
+	    if (parsedSunriseHours < 10) {
+	      parsedSunriseHours = "0" + parsedSunriseHours;
+	    }
+	  var parsedSunriseMins = new Date(response.sys.sunrise*1000).getMinutes();
+	    if (parsedSunriseMins < 10) {
+	      parsedSunriseMins = "0" + parsedSunriseMins;
+	    }
+	  var parsedSunsetHours = new Date(response.sys.sunset*1000).getHours();
+	    if (parsedSunsetHours < 10) {
+	      parsedSunsetHours = "0" + parsedSunsetHours;
+	    }
+	  var parsedSunsetMins = new Date(response.sys.sunset*1000).getMinutes();
+	    if (parsedSunsetMins < 10) {
+	      parsedSunsetMins = "0" + parsedSunsetMins;
+	    }
+	  var formatSunrise = parsedSunriseHours + ":" + parsedSunriseMins;
+	  var formatSunset = parsedSunsetHours + ":" + parsedSunsetMins; 
+	  // Next: Make accept other time zones
+
+	  $('.sunrise').text(formatSunrise);
+	  $('.sunset').text(formatSunset);
+	  $('.sun-data').removeClass('hide');
+	  });
+}
 
 // to do
 
+// make search box start left. right now noticeable lag
+//geolocation
 // proper time zones
 // idea: find language of searched country, return translated data (and can toggle lang)
